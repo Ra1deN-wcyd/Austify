3d7242820bfc14e614ebea2c11c26acf65e34ea2
Tamjid.-wcyd <tamjidahmed0@gmail.com>
2026-04-03 10:18:42 -0700

    Revert "frontend shofted to react"

--- resources/js/pages/Collaborations.jsx
+++
@@ -1,8 +0,0 @@
-export default function Collaborations() {
-    return (
-        <div className="container mt-4">
-            <h4>Collaborations</h4>
-            <p className="text-muted">Coming soon...</p>
-        </div>
-    );
-}
