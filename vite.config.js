7ef980534fcdaaf203b1de6ecc8cb423d2001f26
TAWSIF-ABIR <md.tawsifarifeen@gmail.com>
2026-04-04 23:34:30 +0600

    fix package-lock conflict

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '127.0.0.1',
+    },
     plugins: [
         laravel({
             input: ['resources/js/app.jsx'],
